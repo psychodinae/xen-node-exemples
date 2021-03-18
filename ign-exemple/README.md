d5bf45104a2f2064394560741fa14e8fc70de65c
mellon_collie <noteprof213@gmail.com>
2021-03-18 16:42:28 -0300

    inital commit

---
+++ ign-exemple/README.md
@@ -0,0 +1,3 @@
+# ign-exemple
+
+Exemple to get firefox cookie to login on ignboards forum because this site not use standard xenforo login sistem.
---
+++ ign-exemple/index.js
@@ -0,0 +1,15 @@
+const XenNode = require("xen-node");
+const FfCookieLoader = require("./ff_cookie_loader.js");
+
+// exemple to get firefox cookie to login on ignboards forum because 
+// this site not use standard xenforo login sistem.
+
+const url = "https://www.ignboards.com";
+const ign = new XenNode(url);
+
+FfCookieLoader((ck) => {
+  ign.checkLogin(ck)
+    .then((resp) => console.log("logged:", resp))
+    .then(() => ign.react("3", "525722349"))
+    .catch((err) => console.log(err));
+});
