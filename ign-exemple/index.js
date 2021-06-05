58d79215a9910e727c40035f886f1db2a92c0563
mellon_collie <67171012+psychodinae@users.noreply.github.com>
2021-06-05 08:18:57 -0300

    Update index.js

--- ign-exemple/index.js
+++ ign-exemple/index.js
@@ -2,7 +2,7 @@ const XenNode = require("xen-node");
 const FfCookieLoader = require("./ff_cookie_loader.js");
 
 // exemple to get firefox cookie to login on ignboards forum because 
-// this site not use standard xenforo login sistem.
+// this site not use standard xenforo login system.
 
 const url = "https://www.ignboards.com";
 const ign = new XenNode(url);
